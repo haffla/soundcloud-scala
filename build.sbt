a8624711089e5452477632af92f64214d39a1c3e
haffla <jakob.pupke@googlemail.com>
2015-10-23 00:25:08 +0200

    init

---
+++ build.sbt
@@ -0,0 +1,47 @@
+name := "Soundcloud Scala"
+
+organization := "org.haffla"
+
+version := "0.2-SNAPSHOT"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+  "net.databinder.dispatch" %% "dispatch-core" % "0.11.3",
+  "net.liftweb" %% "lift-json" % "2.6+"
+)
+
+publishTo := {
+  val nexus = "https://oss.sonatype.org/"
+  if (isSnapshot.value)
+    Some("snapshots" at nexus + "content/repositories/snapshots")
+  else
+    Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+}
+
+publishArtifact in Test := false
+
+publishTo := Some(Resolver.file("file",  new File( "/home/jacke/.ivy2/local" )) )
+
+pomIncludeRepository := { _ => false }
+
+pomExtra :=
+  <url>https://github.com/haffla/soundcloud-scala</url>
+    <licenses>
+      <license>
+        <name>BSD-style</name>
+        <url>http://www.opensource.org/licenses/bsd-license.php</url>
+        <distribution>repo</distribution>
+      </license>
+    </licenses>
+    <scm>
+      <url>git@github.com:haffla/soundcloud-scala.git</url>
+      <connection>scm:git@github.com:haffla/soundcloud-scala.git</connection>
+    </scm>
+    <developers>
+      <developer>
+        <id>haffla</id>
+        <name>Jakob Pupke</name>
+        <url>https://github.com/haffla</url>
+      </developer>
+    </developers>
---
+++ src/main/scala/org/haffla/soundcloud/Client.scala
@@ -0,0 +1,78 @@
+package org.haffla.soundcloud
+
+import dispatch.Defaults._
+import dispatch._
+import net.liftweb.json._
+
+
+object Client {
+  def apply(clientId:String) = new Client(clientId)
+  def apply(clientId:String, clientSecret:String, redirectUri:String) = new Client(clientId, Some(clientSecret), Some(redirectUri))
+}
+
+class Client(clientId:String, clientSecret:Option[String] = None, redirectUri:Option[String] = None, host:String = "http://api.soundcloud.com") {
+
+  type anyMap = Map[String,Any]
+  
+  val authParameters = (clientSecret, redirectUri) match {
+    case (Some(secret), Some(uri)) =>
+      Some(
+        Map(
+        "grant_type" -> "authorization_code",
+        "redirect_uri" -> uri,
+        "client_id" -> clientId,
+        "client_secret" -> secret
+        )
+      )
+    case _ => None
+  }
+
+  def users(id:String)(subResource:String = "", limit:Int = -1) = {
+    doRequest("users", id)(subResource, limit)
+  }
+
+  def tracks(id:String)(subResource:String = "", limit:Int = -1) = {
+    doRequest("tracks", id)(subResource, limit)
+  }
+
+  def authorizeUrl():String = {
+    redirectUri match {
+      case Some(uri) => uri
+      case None => throw new Exception("You've initialized the client without redirect_uri")
+    }
+  }
+
+  def exchange_token(code:String):Future[Map[String,String]] = {
+    val params = authParameters match {
+      case None =>
+        throw new Exception(
+          "In order to authenticate against Soundcloud you have to provide your client_secret and a redirect_uri")
+      case _ =>
+        authParameters.get + ("code" -> code)
+    }
+    val request = url(host + "/oauth2/token") << params
+    val response = Http(request OK as.String)
+    for (r <- response)
+      yield parse(r).values.asInstanceOf[Map[String,String]]
+  }
+
+  private def doRequest(resource:String, id:String)(subResource:String, limit:Int) = {
+    val _u = host + s"/$resource/$id/$subResource?client_id=$clientId"
+    val _url = limit match {
+      case -1 => _u
+      case _ => _u + s"&limit=$limit"
+    }
+    val request = url(_url)
+    val response = Http(request OK as.String)
+    for (r <- response)
+      yield {
+        val json = parse(r).values
+        subResource match {
+          case s if s.matches("^[a-z][a-z]*$") =>
+            json.asInstanceOf[List[anyMap]]
+          case _ =>
+            json.asInstanceOf[anyMap]
+        }
+      }
+  }
+}
